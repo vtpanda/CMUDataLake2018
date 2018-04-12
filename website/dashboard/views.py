45e890e3d5947eb1fc77b2fc8e1e299392795f5c
Dan Hou <31412607+doradhou@users.noreply.github.com>
2018-04-12 00:23:46 -0400

    front end index page

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
 from pyathena import connect
-#table: browse_condition(healthy), eligibilities(gender), countries(country), age(optional),
+
+
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+
+# table: browse_condition(healthy), eligibilities(gender), countries(country), age(optional),
 def connectAthena(request, tablename):
-    cursor = connect(aws_access_key_id='AKIAICRP6ONOPQ2DH2EA',
-                 aws_secret_access_key='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',
-                 s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
-                 region_name='us-east-2').cursor()
+    cursor = connect(aws_access_key_id='',
+                     aws_secret_access_key='',
+                     s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
+                     region_name='us-east-2').cursor()
     query = "select * from clinic." + tablename + " limit 10"
     cursor.execute(query)
     rows = []
     for row in cursor:
         rows.append(row)
         rows.append("\n")
-    context = {'rows' : rows}
-    return render(request, 'home.html', context)
+    context = {'rows': rows}
+    return render(request, 'dashboard/patient-home.html', context)
+
+
 # Create your views here.
 def home(request):
     if request.method == "GET":
-        return render(request, 'home.html',{})
+        return render(request, 'dashboard/patient-home.html', {})
     if request.method == 'POST':
         tablename = request.POST['tablename']
-        cursor = connect(aws_access_key_id='AKIAICRP6ONOPQ2DH2EA',
-                         aws_secret_access_key='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',
+        cursor = connect(aws_access_key_id='',
+                         aws_secret_access_key='',
                          s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
                          region_name='us-east-2').cursor()
         query = "select * from clinic." + tablename + " limit 10"
@@ -31,5 +39,4 @@ def home(request):
             rows.append(row)
             rows.append("\n")
         context = {'rows': rows}
-        return render(request, 'home.html', context)
-
+        return render(request, 'dashboard/patient-home.html', context)
