d9ad98cbeddc17eef8a6040e662942a3f4e69e4d
yuhengwang <yuhengw@andrew.cmu.edu>
2018-04-11 23:18:50 -0400

    Update views.py

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from pyathena import connect
 #table: browse_condition(healthy), eligibilities(gender), countries(country), age(optional),
 def connectAthena(request, tablename):
-    cursor = connect(aws_access_key_id='AKIAICRP6ONOPQ2DH2EA',
-                 aws_secret_access_key='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',
+    cursor = connect(aws_access_key_id='',
+                 aws_secret_access_key='',
                  s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
                  region_name='us-east-2').cursor()
     query = "select * from clinic." + tablename + " limit 10"
