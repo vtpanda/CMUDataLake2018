97bb733a9d7c3b7c9ab8299669ffc2eb827f6d90
huyingfan <yingfanh@andrew.cmu.edu>
2018-05-11 16:24:20 -0400

    final update

--- website/dashboard/views.py
+++ website/dashboard/views.py
@@ -5,6 +5,7 @@ import numpy as np
 from dashboard.ConnectSagemaker import invoke_sagemake_endpoint
 from decimal import getcontext, Decimal
 
+# set global variables
 unique_countries = []
 unique_conditions = []
 unique_interventions = []
@@ -18,38 +19,6 @@ def index(request):
     return render(request, 'dashboard/index.html')
 
 
-def readAccessKey():
-    return 0
-
-
-def readSecretKey():
-    return 0
-
-
-# reported event:
-# table: browse_condition(healthy), eligibilities(gender), countries(country), age(optional),
-
-
-def getColumnName(tablename):
-    query = "select column_name from information_schema.columns where table_name = " + "'" + tablename + "'"
-    cursor = connect(aws_access_key_id=os.environ["accessKey"],
-                     aws_secret_access_key=os.environ["secretKey"],
-                     s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
-                     region_name='us-east-2').cursor()
-    cursor.execute(query)
-    columnName = []
-    for row in cursor:
-        line = str(row)
-        print(line)
-        start = line.index("'", 0, len(line))
-        end = line.index("'", start + 1, len(line))
-        columnName.append(line[start + 1: end])
-    print(columnName)
-    return columnName
-
-
-# Create your views here.
-
 def home(request):
     global unique_countries, unique_conditions, unique_interventions, cursor
     if len(unique_countries) == 0:
@@ -61,33 +30,6 @@ def home(request):
                       {"show": False, "conditions": unique_conditions, "countries": unique_countries,
                        "interventions": unique_interventions})
     if request.method == 'POST':
-        # cursor = connect(aws_access_key_id=os.environ["accessKey"],
-        #                  aws_secret_access_key=os.environ["secretKey"],
-        #                  s3_staging_dir='s3://aws-athena-query-results-565635975808-us-east-2/',
-        #                  region_name='us-east-2').cursor()
-
-        # if 'tablename' in request.POST:
-        #     tablename = request.POST['tablename']
-        #     columnNames = getColumnName(tablename)
-        #     attributeLine = []
-        #     for column in columnNames:
-        #         query = "select " + column + " from clinic." + tablename + " limit 10"
-        #         print(query)
-        #         cursor.execute(query)
-        #         attributes = []
-        #         for row in cursor:
-        #             line = str(row)
-        #             start = -1
-        #             end = len(line)
-        #             if line.find("'") != -1:
-        #                 start = line.index("'", 0, len(line))
-        #                 end = line.index("'", start + 1, len(line))
-        #             attributes.append(line[start + 1: end])
-        #         attributeLine.append(attributes)
-        #     a = map(list, zip(*attributeLine))
-        #     context = {"tablename": tablename, "columnNames": columnNames, "attributeLine": a, "show": True}
-        #     return render(request, 'dashboard/patient-home.html', context)
-        # else:
         condition = request.POST.getlist('health_condition[]')
         gender = request.POST['gender']
         country = request.POST['country']
@@ -117,8 +59,6 @@ def home(request):
         conditionlist = conditionlist[0: len(conditionlist) - 1]
         conditionlist += ')'
 
-        print("HERE COMES THE EAD!!!!!!")
-        print(conditionlist)
         query = "select s.nct_id, brief_title, array_agg(distinct adverse_event_term) adverse_event" \
                 + " from clinic.studies s" \
                 + " join clinic.reported_events r" \
@@ -146,7 +86,6 @@ def home(request):
                 + " on s.nct_id = e.nct_id" \
                 + " group by s.nct_id, brief_title" \
                 + " limit 10;"
-        print(query)
         cursor.execute(query)
         columnNames = ['nct_id', 'brief_title', 'adverse_events']
         attributeLine = []
@@ -159,7 +98,7 @@ def home(request):
 
         endpoint_name = 'yangz5test'
         percentage = invoke_sagemake_endpoint(vector, endpoint_name) * 100
-        print(percentage)
+
         percentage = Decimal(percentage).quantize(Decimal('0.00'))
 
         context = {"tablename": 'result table', "columnNames": columnNames, "attributeLine": attributeLine,
